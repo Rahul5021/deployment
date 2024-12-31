ea99885b6623b069b582f14d0b0f7ee957bd3a67
Rahul Agrawal <agrawal.rahul.1025@gmail.com>
2024-12-31 21:25:12 +0545

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
+
 application = Flask(__name__,static_folder='static')
 app = application
 
